8559d4d9fdc20a57963ee7ad002b4b5529c52dff
Scyu <scyu@scyu.dev>
2021-03-22 22:08:42 +0000

    Optimizations

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -2,6 +2,7 @@ import { ClientEvents } from 'discord.js';
 import DB from '../utils/database';
 import PolymartAPI from '../utils/polymartAPI';
 import consola from 'consola';
+import groupBy from 'lodash/groupBy';
 
 module.exports = async (...args: ClientEvents['guildMemberAdd']) => {
     const [member] = args;
@@ -20,23 +21,27 @@ module.exports = async (...args: ClientEvents['guildMemberAdd']) => {
         const userID = await userDB.get('userID');
         let validResources = 0;
 
-        if (await guildDB.has('resources') && await guildDB.get('resources') !== null) {
+        if (await guildDB.has('resources')) {
             const resources = await guildDB.get('resources');
 
+            const userData = await PolymartAPI.getUserData(userID, apiKey);
+            if (userData === null) return;
+
+            const userResourceData = groupBy(userData.resources, 'id');
+
             for (const rID in resources) {
-                const resourceUserData = await PolymartAPI.getResourceUserData(rID, userID, apiKey);
-                const userData = await PolymartAPI.getUserData(userID, apiKey);
-                consola.debug(userData);
-                if (resourceUserData === null) return;
-                if (resourceUserData.purchaseValid) {
+                const resourceInfo = resources[rID];
+
+                if (userResourceData[resourceInfo.id][0]['purchaseValid']) {
                     validResources++;
 
-                    if ('role' in resources[rID]) {
+                    if ('role' in resourceInfo) {
                         try {
-                            await member.roles.add(resources[rID]['role']);
+                            await member.roles.add(resourceInfo['role']);
                         } catch (e) {
                             consola.error(e);
                         }
+
                     }
                 }
             }
